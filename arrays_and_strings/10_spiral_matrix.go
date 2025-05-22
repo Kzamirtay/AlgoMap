c116f343d6385d62c2a8cacd4821573ce0c5dac9
Dinmukhammed Amirtay <kzamirtay@gmail.com>
2025-05-22 21:25:11 +0500

    spiral matrix v3

--- arrays_and_strings/10_spiral_matrix.go
+++ arrays_and_strings/10_spiral_matrix.go
@@ -10,23 +10,38 @@ func spiralOrder(matrix [][]int) []int {
 
 	for {
 		answer = append(answer, matrix[rowI][colI])
-		colI++
-		if rowI == up && colI == right {
-			// Убираем Верх
-			up++
+		if rowI == up {
+			if colI == right-1 {
+				// Убираем Верх
+				up++
+				rowI++
+			} else {
+				colI++
+			}
+		} else if colI == right-1 {
+			if rowI == down-1 {
+				// Убираем Право
+				right--
+				colI--
+			} else {
+				rowI++
+			}
+		} else if rowI == down {
+			if colI == left {
+				// Убираем Низ
+				down--
+			} else {
+				colI--
+			}
 		}
-		if rowI == down && colI == right {
-			// Убираем Право
-			right--
-		}
-		if rowI == down && colI == left {
-			// Убираем Низ
-			down--
-		}
-		if rowI == up && colI == left {
-			// Убираем Низ
-			left++
-		}
+		//if colI == left {
+		//	if rowI == up {
+		//		// Убираем Низ
+		//		left++
+		//	} else {
+		//		rowI++
+		//	}
+		//}
 	}
 	return answer
 }
