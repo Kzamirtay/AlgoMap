ecad11ec4653751c9a61105e763c150c91fa039d
Dinmukhammed Amirtay <kzamirtay@gmail.com>
2025-05-24 20:12:32 +0500

    Roman to Integer change

--- arrays_and_strings/03_roman_to_integer.go
+++ arrays_and_strings/03_roman_to_integer.go
@@ -1,41 +1,26 @@
 package arrays_and_strings
 
 func romanToInt(s string) int {
-	cat := map[string]int{
-		"I": 1,
-		"V": 5,
-		"X": 10,
-		"L": 50,
-		"C": 100,
-		"D": 500,
-		"M": 1000,
+	cat := map[rune]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
 	}
 
-	answer := 0
-	lenS := len(s)
-	current := 0
-	next := 0
-	nextUsed := false
+	str := []rune(s)
+	lastI := len(s) - 1
+	answer := cat[str[lastI]]
 
-	for i, letter := range s {
-		next = 0
-		if nextUsed {
-			nextUsed = false
-			continue
+	for i := lastI; i > 0; i-- {
+		if cat[str[i]] > cat[str[i-1]] {
+			answer -= cat[str[i-1]]
+		} else {
+			answer += cat[str[i-1]]
 		}
-
-		current = cat[string(letter)]
-
-		if lenS-1 != i {
-			next = cat[string(s[i+1])]
-		}
-
-		if current < next {
-			answer += next - current
-			nextUsed = true
-			continue
-		}
-		answer += current
 	}
 
 	return answer
